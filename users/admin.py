8b160f277920d5e3c4e7c22a21b776ca10c16ede
Bloodimirg <bloodimirg@gmail.com>
2024-11-09 23:44:08 +0300

    админка пользователей

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """Админка пользователей"""
+
+    list_display = ("pk", "email", "role")
