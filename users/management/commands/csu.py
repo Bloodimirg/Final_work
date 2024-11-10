a387d3f3483d2724e812b7e1ddd291bb980868a0
Bloodimirg <bloodimirg@gmail.com>
2024-11-11 00:53:30 +0300

    Переменные окружения вынесены для автоматического создания администратора

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,3 +1,5 @@
+import os
+
 from django.core.management import BaseCommand
 from users.models import User
 
@@ -7,8 +9,8 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Создание админа"""
-        if not User.objects.filter(email="").exists():
-            user = User.objects.create_superuser(email="", password="")
+        if not User.objects.filter(email=os.getenv("ADMIN_EMAIL")).exists():
+            user = User.objects.create_superuser(email=os.getenv("ADMIN_EMAIL"), password=os.getenv("ADMIN_PASSWORD"))
             user.save()
             self.stdout.write(
                 self.style.SUCCESS(f"Суперпользователь {user.email} успешно создан")
